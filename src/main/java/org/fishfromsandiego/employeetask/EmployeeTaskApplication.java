8ff93c3fa93d28ca7b9c8b4bbcac95e43500dcfa
Maksim Petrov <petrov.maksim5@mail.ru>
2025-07-15 18:21:57 +0300

    init commit

---
+++ src/main/java/org/fishfromsandiego/employeetask/EmployeeTaskApplication.java
@@ -0,0 +1,13 @@
+package org.fishfromsandiego.employeetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeTaskApplication.class, args);
+    }
+
+}
